8fb4ecdbb9d52b29cb245ee16e3b0e9614418105
kwest-ng <a.simple.kwest@gmail.com>
2023-11-14 20:03:09 -0800

    Initial commit, auto-PR is untested

---
+++ README.md
@@ -0,0 +1,39 @@
+# haskell-dev-tools
+
+_copied from fossas/haskell-dev-tools_
+
+This repo serves two purposes:
+
+- Creating the docker image that is used to run the linter and formatter in
+  the CI builds for my haskell projects.
+- Automatically creating PRs for the image when the tools are updated.
+
+Most of this repo is in the `.github/workflows` folder, but the two special
+items are the Dockerfile, and the script that updates it: `update-dev-tools.sh`.
+
+The image that is built is scoped to this repo as well as the org, but is made
+publicly available, since there's no secrets or magic, and it makes docker auth
+easier in spectrometer's CI. On that note, the code in this repo is released
+warranty-free into the public domain, under [The Unlicense](LICENSE).
+
+The image is published as `ghcr.io/fossas/haskell-dev-tools:{version}-{hash}`, where
+`version` is the GHC compiler version we target, and `hash` is the git hash of the
+commit used to build the image.
+
+## Updating the container
+
+### How to update the version of a tool in the container
+
+Don't.  This repo automatically updates tools to their latest versions, and
+will override ANY version change in the dockerfile.
+
+### How to add a new tool to the container
+
+Update the `update-dev-tools.sh` file and add the tool you're interested in.
+Then, add it to the install command in the dockerfile.  Match the format of
+the existing tools.
+
+You can enable auto-merge on your PR, and it will automatically publish to
+the container registry after about 1 hour (due to long build times).
+
+Once it's published, you can use it immediately.
---
+++ src/update-dev-tools.sh
@@ -0,0 +1,66 @@
+#! /bin/bash -e
+
+# Don't source this file or this line won't work.
+dockerfile="$(dirname "$0")/Dockerfile"
+
+get_versions () {
+    # name=$1, json_file=$2
+    curl --show-error --silent --location --header 'Accept: application/json' "https://hackage.haskell.org/package/${1}/preferred" > "$2"
+}
+
+extract_latest_version () {
+    # json_file=$1, version_file=$2
+    jq --raw-output '.["normal-version"] | .[0]' "$1" > "$2"
+}
+
+edit_dockerfile () {
+    # name=$1, version=$2
+    sed --regexp-extended --in-place "s/$1-[0-9\.]+/$1-$2/" "$dockerfile"
+}
+
+update_package () {
+    package_name=$1
+    temp_dir=$2
+    _prefix="${temp_dir}/${package_name}"
+    json_file="${_prefix}.json"
+    version_file="${_prefix}.version"
+
+    get_versions "$package_name" "$json_file"
+    extract_latest_version "$json_file" "$version_file"
+
+    # Don't use cat when not needed
+    version=$(< "$version_file")
+
+    edit_dockerfile "$package_name" "$version"
+}
+
+echoerr () {
+    echo "$@" 1>&2
+}
+
+log () {
+    echoerr "$@"
+}
+
+execute () {
+    log "Creating tempdir"
+    _tempdir=$(mktemp --directory)
+    log "Backing up Dockerfile for diffing"
+    cp "$dockerfile" "${dockerfile}.bak"
+    log "Updating hlint"
+    update_package hlint "$_tempdir"
+    log "Updating fourmolu"
+    update_package fourmolu "$_tempdir"
+    log "Updating cabal-fmt"
+    update_package cabal-fmt "$_tempdir"
+    log "Updating hadolint"
+    update_package hadolint "$_tempdir"
+    # We don't care about the exit code, we just want the diff output
+    log "Running diff to check for changes"
+    diff "${dockerfile}.bak" "$dockerfile" || true
+    log "Cleaning up temp files"
+    rm --recursive --force "$_tempdir"
+    log "Done."
+}
+
+execute
